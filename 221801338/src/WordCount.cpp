05c86268d79eb892fcc2f356ba7be3481a3f944f
fhao1999 <79969258+fhao1999@users.noreply.github.com>
2021-03-05 00:34:15 +0800

    修改WordCount.cpp文件
    之前将结构体放入了.h文件，但是程序出了些问题，正在调试，所以还是先将结构体放回WordCount.cpp文件

--- 221801338/src/WordCount.cpp
+++ 221801338/src/WordCount.cpp
@@ -1,4 +1,52 @@
+/**
+作者：221801338_冯浩
+最后修改时间：2020/3/4
+**/
 #include "Lib.h"
+
+
+map<string, int> mapword1;
+
+
+void loadword(char w[])
+{
+	string wr;
+	wr = w;
+	map<string, int>::iterator it1 = mapword1.find(wr);//在map红黑树中查找单词 
+	if (it1 == mapword1.end())
+		mapword1.insert(pair<string, int>(wr, 1));//未找到单词，插入单词并设定频次为1 
+	else
+		++it1->second;//找到单词，单词出现频次增加 
+}
+
+
+struct sWord
+{
+	string w;
+	int count;
+
+};//定义一个用于存放单词及频次的结构体 
+
+void merge(sWord* a, sWord* c, int l, int mid, int r) {
+	int i = l, j = mid + 1, m = 1;
+	while (i <= mid && j <= r) {
+		if (a[i].count < a[j].count) c[m++] = a[j++];
+		else c[m++] = a[i++];
+	}
+	while (i <= mid) c[m++] = a[i++];
+	while (j <= r) c[m++] = a[j++];
+	for (int k = 1; k <= r - l + 1; k++)
+		a[l + k - 1] = c[k];
+}
+
+void sort(sWord* a, sWord* c, int l, int r) {
+	if (l < r) {
+		int mid = (l + r) / 2;
+		sort(a, c, l, mid);
+		sort(a, c, mid + 1, r);
+		merge(a, c, l, mid, r);
+	}
+}
 
 int main(int argc, char* argv[])
 {
