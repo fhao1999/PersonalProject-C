5f46504f24756b6283229bb3e2616ffb71274475
fhao1999 <79969258+fhao1999@users.noreply.github.com>
2021-03-04 21:02:37 +0800

    修改Lib.h
    将结构体与类全放入头文件中

--- 221801338/src/Lib.h
+++ 221801338/src/Lib.h
@@ -29,5 +29,17 @@ private:
 
 };
 
+static map<string, int> mapword1;
 
+void loadword(char w[]);
+
+struct sWord
+{
+	string w;
+	int count;
+
+};//定义一个用于存放单词及频次的结构体 
+
+void merge(sWord* , sWord* , int , int , int );
+void sort(sWord* , sWord* , int , int);
 #endif //LIB_H
