9455adf5f94726fbcbb199846c9d627d8b3595d5
fhao1999 <79969258+fhao1999@users.noreply.github.com>
2021-03-04 20:19:30 +0800

    完善Lib.h
    上一版未使用.h文件，只是将其创建。

--- 221801338/src/Lib.h
+++ 221801338/src/Lib.h
@@ -2,6 +2,32 @@
 #ifndef LIB_H
 #define LIB_H
 
-// TODO: 添加要在此处预编译的标头
+#include <iostream>
+#include<string>
+#include<fstream>
+#include<cassert>
+#include<map> 
+#include<cstdlib>
+#include<time.h> 
+#define MAXN 10000000
+using namespace std;
+class testfile
+{
+public:
+	testfile countcha(char*, testfile);//计算字符数
+	testfile countword(char*, testfile);//计算单词数
+	testfile countline(char*, testfile);//计算行数
+	int getcharacters();
+	int getlines();
+	int getwords();
+	char* content;//存放文本文件数据 
+	void init();
+private:
+	int characters;
+	int words;
+	int lines;
+
+};
+
 
 #endif //LIB_H
